731a115f8d286187779fc96acc5366b9f85033d7
stormfather0 <ivan.ystskovyna@gmail.com>
2025-08-07 16:40:10 +0300

    test

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/my-app/', // Your GitHub repo name with trailing slash
-  plugins: [react(), tailwindcss()],
+  base: '/my-app/', // Must match your GitHub repo name with trailing slash
+  plugins: [react()],
 })
