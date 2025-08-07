e4d4295fe0633152990111593191a07fa2f11a71
stormfather0 <ivan.ystskovyna@gmail.com>
2025-08-07 12:33:21 +0300

    update naming to avoid being flagged by google as a malicious website

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/react-app/', 
+  base: '/my-app/', 
   plugins: [react(),
     // tailwindcss(),
   ],
