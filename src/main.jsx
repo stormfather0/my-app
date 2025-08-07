2b8fcb6f54d3c78614f029b41d570a2fe8105dc1
stormfather0 <ivan.ystskovyna@gmail.com>
2025-08-07 14:25:42 +0300

    test

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import 'index.css'
+
 import { BrowserRouter as Router } from 'react-router-dom'
 import { CartProvider } from './CartContext'
 
