a9715657554a559dfb8bb194bf24060baf8d8b09
stormfather0 <ivan.ystskovyna@gmail.com>
2025-08-13 12:48:36 +0300

    fix 404 product links

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-
-import { BrowserRouter as Router } from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom'  // <-- change here
 import { CartProvider } from './CartContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <CartProvider>
-      <Router basename="/my-app"> 
+      <Router basename="/my-app">  {/* you can keep the basename */}
         <App />
       </Router>
     </CartProvider>
