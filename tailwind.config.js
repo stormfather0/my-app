89ec108503ba8fea382637b23e78ad0c55ead68c
stormfather0 <ivan.ystskovyna@gmail.com>
2025-08-13 11:57:51 +0300

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 // tailwind.config.js
-module.exports = {
-    content: [
-      "./index.html",
-      "./src/**/*.{js,jsx,ts,tsx}",
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  };
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/my-app/', // Must match your GitHub repo name with trailing slash
+  base: '/my-app/', 
   plugins: [react()],
 })
