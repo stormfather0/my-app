1be9ac8404768568b613351098098a399a7d155c
stormfather0 <ivan.ystskovyna@gmail.com>
2025-08-13 12:59:15 +0300

    fix links

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 
-import { HashRouter as Router } from 'react-router-dom'  
-import { CartProvider } from './CartContext'
+import { HashRouter as Router } from 'react-router-dom'; // <-- use HashRouter
+import { CartProvider } from './CartContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <CartProvider>
-      <Router basename="/my-app">  
+      <Router>
         <App />
       </Router>
     </CartProvider>
   </React.StrictMode>
-)
+);
